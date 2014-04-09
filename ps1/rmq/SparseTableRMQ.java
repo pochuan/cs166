bbec9ba4d5d01a554d278fd98730c0697732dd19
Rachel Fenichel <fenichr@gmail.com>
2014-04-08 18:50:57 -0700

    Sparse Table
    Implemented and tested the Sparse Table RMQ data structure.

--- ps1/rmq/SparseTableRMQ.java
+++ ps1/rmq/SparseTableRMQ.java
@@ -6,6 +6,10 @@ package rmq;
  * You will implement this class for problem 3.ii of Problem Set One.
  */
 public class SparseTableRMQ implements RMQ {
+    int[][] sparseTable;
+    int[] fastLogs;
+    float[] origElems;
+    
     /**
      * Creates a new SparseTableRMQ structure to answer queries about the
      * array given by elems.
@@ -13,7 +17,50 @@ public class SparseTableRMQ implements RMQ {
      * @elems The array over which RMQ should be computed.
      */
     public SparseTableRMQ(float[] elems) {
-        // TODO: Implement this!
+        origElems = elems;
+        
+        int length = elems.length;
+        populateFastLogs(length);
+        
+        // Based on pseudocode in the topcoder tutorial at
+        // http://www.topcoder.com/tc?d1=tutorials&d2=lowestCommonAncestor&module=Static
+        sparseTable = new int[length][fastLogs[length] + 1];
+        // Initialize for the intervals with length 1
+        for (int i = 0; i < length; i++) {
+            sparseTable[i][0] = i;
+        }
+        // Compute values from smaller to bigger intervals
+        for (int j = 1; (1 << j) <= length; j++) {
+            for (int i = 0; (i + (1 << j) - 1) < length; i++) {
+                int sparse1 = sparseTable[i][j - 1];
+                int sparse2 = sparseTable[i + (1 << (j - 1))][j - 1];
+                if (elems[sparse1] < elems[sparse2]) {
+                    sparseTable[i][j] = sparse1;
+                }
+                else {
+                    sparseTable[i][j] = sparse2;
+                }
+            }
+        }        
+    }
+    
+    private void populateFastLogs(int length) {
+        fastLogs = new int[length + 1];
+        int counter = 0;
+        int log = 0;
+        int num = 1;
+        
+        fastLogs[0] = 0;
+        
+        for (int i = 1; i < fastLogs.length; i++) {
+            counter++;
+            fastLogs[i] = log;
+            if (counter == num) {
+                log++;
+                num *= 2;
+                counter = 0;
+            }
+        }
     }
 
     /**
@@ -22,7 +69,12 @@ public class SparseTableRMQ implements RMQ {
      */
     @Override
     public int rmq(int i, int j) {
-        // TODO: Implement this!
-        return -1;
+        int log = fastLogs[j - i + 1];
+        int sparse1 = sparseTable[i][log];
+        int sparse2 = sparseTable[j - (int)Math.pow(2, log) + 1][log];
+        if (origElems[sparse1] < origElems[sparse2]) {
+            return sparse1;
+        }
+        return sparse2;
     }
 }
