bc358d584abd5343eccdc4c7c419eba0a18d299e
Paul Chen <pochuan@corn06.stanford.edu>
2014-04-16 00:38:51 -0700

    wrote is_red_black_tree

--- ps2/rbtree.c
+++ ps2/rbtree.c
@@ -27,11 +27,33 @@ struct node* left(struct node* leftPointer) {
  *    (though the left pointers may need to be tweaked a bit to get to
  *    valid data).
  *
- * TODO: Edit this comment to describe why this function runs in time O(n).
+ * This function runs in time O(n) since it performs one check for the root 
+ * is black, which is O(1). Then it does depth first search to check that each 
+ * root leaf path has the same number of black nodes and that red nodes don't
+ * have red children. The Depth first search only processes each of the n nodes 
+ * once, doing constant work each time. Thus, the runtime is O(n). 
  */
 bool is_red_black_tree(struct node* root) {
-	/* TODO: Implement this! */
-	return false;
+    if (red(root)) return false;
+    return (blackHeight(root, false) != -1);
+}
+
+// Returns the black height of a red black tree if it is valid
+// Returns -1 if root leaf path has differend number of black nodes or if a red
+// node has red children.
+int blackHeight(struct node* node, bool parentRed) {
+    if (node == NULL) return 0;
+    bool isRed = red(node);
+    if (isRed && parentRed) {
+        return -1;
+    }
+    int currBlack = (isRed) ? 0 : 1;
+    int lbHeight = blackHeight(left(node->left), isRed);
+    int rbHeight = blackHeight(node->right, isRed);
+    if ((lbHeight == -1) || (rbHeight == -1) || (lbHeight != rbHeight)) {
+        return -1;
+    }
+    return lbHeight + currBlack;
 }
 
 /**
