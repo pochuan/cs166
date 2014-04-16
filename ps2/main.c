63f19cd46462fc2597e062bd360804ee7f8741c4
fenichel <fenichr@stanford.edu>
2014-04-16 02:12:17 -0700

    Test code
    Added a function that creates a red-black tree from a sorted array of the given size and then checks it it was actually a red-black tree.

--- ps2/main.c
+++ ps2/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include "rbtree.h"
 
+#define MAX_TEST_SIZE 100
+int elems[MAX_TEST_SIZE];
+
+void test_red_black(int size) {
+    printf("Making a tree of size %i...\n", size);
+    struct node* rbroot = to_red_black_tree(elems, size);
+    printf("Checking if the tree was well-formed\n");
+    if (is_red_black_tree(rbroot)) {
+        printf("PASS\n");
+    }
+    else {
+        printf("\tFAIL\n")
+    }
+}
+
 int main(int argc, char *argv[]) {
     printf("Red black trees\n");
+    for (int i = 0; i < MAX_TEST_SIZE; i++) {
+        elems[i] = i;
+    }
 
+    for (int i = 0; i < MAX_TEST_SIZE; i++) {
+        test_red_black(i);
+    }
+    
     return 0;
 }
