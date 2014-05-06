2c54c7fb56088dbe8eaa38663ea5130b18bc6640
Paul Chen <pochuan@corn01.stanford.edu>
2014-05-05 22:26:34 -0700

    find cut

--- ps5/WeightBalancedTree.java
+++ ps5/WeightBalancedTree.java
@@ -11,7 +11,36 @@ public class WeightBalancedTree implements BST {
 	 */
 	public WeightBalancedTree(double[] elements) {
 		// TODO: Implement this!
+
+        double[] test = {1,3,2,5};
+        System.out.println("Found cut at: " + findCut(test, 0, test.length));
 	}
+
+    public int findCut(double[] elems, int start, int end) {
+        if ((end - start) == 1) return start;
+
+        double total = 0;
+        for (int i = start; i < end; ++i) {
+            total += elems[i];
+        }
+
+        double leftSum = 0;
+        double rightSum = total - elems[start];
+        double diff = Math.abs(leftSum - rightSum);
+        int cut = start;
+        for (int i = start+1; i < end; ++i) {
+            leftSum += elems[i-1];
+            rightSum -= elems[i];
+            double currDiff = Math.abs(leftSum - rightSum);
+            if (currDiff < diff) {
+                diff = currDiff;
+                cut = i;
+            }
+            else break;
+        }
+
+        return cut;
+    }
 	
 	/**
 	 * Returns whether the specified key is in the BST.
